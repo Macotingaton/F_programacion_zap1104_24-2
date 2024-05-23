284c818b2879861883d369b4ca3245cb1842ad37
Macotingaton <mirroranonimo@gmail.com>
2024-05-23 08:25:05 -0600

    Programación
    :p

---
+++ Numero absoluto pseint/Valor_absoluto.psc
@@ -0,0 +1,8 @@
+Algoritmo Valor_absoluto
+	Escribir "Ingresa un número"
+	Leer x
+	Si x<0 Entonces
+		x = x*(-1)
+	FinSi
+	Escribir "Tu valor absoluto es:",x
+FinAlgoritmo
---
+++ P005_Operadores Aritmeticos=bibliotecas/P005_Operadores Aritmeticos=bibliotecas.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <locale>
+#include <math.h>
+
+using namespace std;
+int main()
+{
+	double num1, num2, num3, num4, resultado = 0;
+
+	cout << "Digite 4 numeros \n"; cin >> num1 >> num2  >> num3>> num4;
+
+	resultado = ((num1)*((num2)/(sqrt(num3))))/(num4)*(pow(num2,2));
+	
+
+	cout << "El resultado es " << resultado;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
